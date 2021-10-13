c1f991914f2808657bf1a40327eca95a13dcb522
MagicianThirteen <MagicianThirteen@gmail.com>
2021-10-13 18:45:22 +0800

    敌人追踪系统

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,15 +8,30 @@ public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     private NavMeshAgent _findPathagent;
+    private Transform target;
     void Start()
     {
         _findPathagent = GetComponent<NavMeshAgent>();
+        target=GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(UpdatePathFind());
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject player=GameObject.FindGameObjectWithTag("Player");
-        _findPathagent.SetDestination(player.transform.position);
+       
     }
+
+    IEnumerator UpdatePathFind()
+    {
+        float freshRare = 1f;
+        while (target!=null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.y);
+            _findPathagent.SetDestination(targetPosition);
+            yield return new WaitForSeconds(freshRare);
+        }
+    }
+    
+    
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,6 +7,7 @@ public class Projectile : MonoBehaviour
     //行为只有一个：一出生就往前飞
     //属性只有一个：速度（不过每种子弹估计速度不一样，所以还要有设置速度的方法）
     public float Speed = 10;
+    public LayerMask collisionMask;
 
     public void SetSpeed(float velocitySpeed)
     {
@@ -15,6 +16,25 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         //根据方向和移动距离
-        transform.Translate(Vector3.forward*Speed*Time.deltaTime);
+        float moveDistance = Speed * Time.deltaTime;
+        CheckCollision(moveDistance);
+        transform.Translate(Vector3.forward*moveDistance);
+        
+    }
+
+    void CheckCollision(float distance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit,distance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            HitDoing(hit);
+        }
+    }
+
+    void HitDoing(RaycastHit hit)
+    {
+        Debug.Log("当前打到的是："+hit.collider.gameObject.name);
+        Destroy(gameObject);
     }
 }
